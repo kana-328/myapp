e0c8c341f60bc245cb968898dc3a11208e073c2f
波形佳奈 <kana-328@i.softbank.jp>
2020-07-29 16:11:32 +0900

    request_spec記入

---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+RSpec.describe 'Sessions', type: :request do
+
+  describe "GET users/sessions/new" do
+    before do
+      get root_path
+    end
+
+    it 'ログイン画面のリクエストが成功すること' do
+      expect(response.status).to eq 200
+    end
+  end
+end
