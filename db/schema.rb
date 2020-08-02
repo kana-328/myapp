f11af5a25f50c85ddb48ec871c416b6a8c1506ff
波形佳奈 <kana-328@i.softbank.jp>
2020-08-02 11:43:30 +0900

    birthdayカラムをdate型に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_095536) do
+ActiveRecord::Schema.define(version: 2020_08_02_023111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_095536) do
   create_table "pets", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.integer "age"
-    t.datetime "birthday"
+    t.date "birthday"
     t.string "breed"
     t.float "weight"
     t.datetime "created_at", null: false
