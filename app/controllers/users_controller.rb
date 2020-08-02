f80966c7e1ccf42614de5f0971929efade7de1ea
波形佳奈 <kana-328@i.softbank.jp>
2020-08-02 19:49:12 +0900

    ransackにpet.name追加,modalパーシャル化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
     @search = User.ransack(params[:q])
-    @users = @search.result(distinct: true)
+    @users = @search.result.includes(:pets)
   end
 end
