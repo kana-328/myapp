1920773f8473f48f2fd6975174d66f4dc6ee9d31
root <kana-328@i.softbank.jp>
2020-09-29 07:11:16 +0000

    メール送信前にプレビュー表示するように設定

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,14 +3,19 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
     @condition = Condition.find(params[:id])
-
+      pet = @condition.pet_id
+      @pet = Pet.find_by(id: pet)
+      user = @pet.user_id
+      @user = User.find_by(id: user)     
   end
 
   def create
-    @contact = Contact.new(contact_params)
-    @condition = Condition.find(params[:contact][:id])
-    if @contact.save
-      ContactMailer.send_mail(User.first, @condition).deliver
+    contact = Contact.new
+    message = params[:message]
+    condition = Condition.find(params[:contact][:id])
+    if contact.save
+      @mail = ContactMailer.send_mail(condition, message)
+      @mail.deliver
       redirect_to reservations_path
       flash[:notice] = 'メールを送信しました'
     else
@@ -21,7 +26,12 @@ class ContactsController < ApplicationController
 
   private
 
+
+  def preview_information_mail
+    @mail = ContactMailer.information_mail(subject, mail_to, name, message)
+  end
+
   def contact_params
-    params.require(:contact).permit(:name, :message)
+    params.require(:contact).permit(:message)
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,13 @@
 class ContactMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def send_mail(user, condition)
-    @user = user
+  def send_mail(condition, message)
+    @message = message
     @condition = condition
+    pet = @condition.pet_id
+    @pet = Pet.find_by(id: pet)
+    user = @pet.user_id
+    @user = User.find_by(id: user)
     mail(to: @user.email, subject: '今日のご様子')
   end
 end
