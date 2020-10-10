5459b47a7e0210b522f481fbfafdaa138e3fedd5
kana-328 <kana-328@i.softbank.jp>
2020-10-11 01:54:34 +0900

    User.CSVインポート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
       format.csv { send_data @users.generate_csv, filename: "users-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" }
     end
   end
+
+  def import
+    # fileはtmpに自動で一時保存される
+    User.import(params[:file])
+    redirect_to users_path
+  end
 end
